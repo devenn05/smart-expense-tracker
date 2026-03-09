e95433591ba1b3d0b5ea32a33ea56126efdff1a4
devenn05 <rahoolhirlekar.com>
2026-03-09 18:50:11 +0530

    Initialize server configurations. Configured MongoDB Atlas and integrated with .env file. Along with that configured package.json so we can start the app via ts-node-dev.

---
+++ backend/src/index.ts
@@ -0,0 +1,13 @@
+import app from "./app";
+import dotenv from 'dotenv';
+import { connectDB } from "./config/db";
+
+dotenv.config()
+
+const PORT = process.env.PORT || 5000;
+
+connectDB().then(()=>{
+    app.listen(PORT, ()=>{
+        console.log(`🚀 Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
+    })
+})
