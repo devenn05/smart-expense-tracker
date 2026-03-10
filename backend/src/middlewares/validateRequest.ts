855830339d011ae0a6031242ba3291af5ec431f7
devenn05 <rahoolhirlekar.com>
2026-03-10 12:37:36 +0530

    feat(validation): implement dynamic Zod request validation middleware
    - Install zod for type safety and validation
    - Create validateRequest middleware interceptor
    - Define initial validation schemas for auth login and registration

---
+++ backend/src/middlewares/validateRequest.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "../utils/AppError";
+import { ZodObject, ZodError} from 'zod';
+
+// A dynamic middleware that accepts any Zod Schema
+export const validate = (schema: ZodObject) => {
+    (req: Request, res: Response, next: NextFunction) => {
+        try {
+            // Validates the incoming request against the schema provided
+            schema.parse({
+                body: req.body,
+                query: req.query,
+                params: req.params
+            });
+            next();
+        } catch (error) {
+            if (error instanceof(ZodError)){
+                // Formats the messy Zod errors into a clean, single readable string for the frontend
+                const errorMessage = error.issues.map((err) => err.message).join(', ');
+                return next(new AppError(errorMessage, 400));
+            }
+            next(error);
+        }
+    }
+};
---
+++ backend/src/schemas/auth.schema.ts
@@ -0,0 +1,28 @@
+import {z} from 'zod';
+
+export const registerSchema = z.object({
+    body: z.object({
+        name: z
+            .string()
+            .min(2, 'Name must be at least 2 characters long'),
+
+        email: z
+            .email('Invalid email address format'),
+
+        password: z.string()
+            .min(8, 'Password must be atleast 8 characters long.')
+            .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+            .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+            .regex(/[0-9]/, "Password must contain at least one number")
+    })
+});
+
+export const loginSchema = z.object({
+    body: z.object({
+        email: z
+            .email('Invalid email address format'),
+
+            password: z.string()
+            .min(8, 'Password is Required')
+    })
+})
