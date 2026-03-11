18ab85a01352f1a475f11dfbe7289a8cd55b0c99
devenn05 <rahoolhirlekar.com>
2026-03-11 12:03:26 +0530

    chore(frontend): Complete configuration of global state for Authentication and api client regarding to it.
    - Configure Axios API instance with withCredentials for secure cookie transport
    - Implement Axios response interceptor to globally catch 401 Unauthorized errors
    - Wrap root application in Redux Provider

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { store } from "../store/store";
+import { clearCredentials } from "../store/slices/authSlice";
 
 const api = axios.create({
     baseURL: 'http://localhost:5000/api/v1',
@@ -7,5 +9,17 @@ const api = axios.create({
         'Content-Type': 'application/json',
     }
 })
+
+api.interceptors.response.use(
+    (response)=>{
+        return response;
+    },
+    (error)=>{
+        if (error.response && error.response.status === 401){
+            store.dispatch(clearCredentials());
+        }
+        return Promise.reject(error);
+    }
+)
 
 export default api;
